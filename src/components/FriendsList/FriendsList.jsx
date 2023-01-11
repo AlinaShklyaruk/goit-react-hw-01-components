e822fb4b0ac592793329eb70c55816431c7050d5
AlinaShklyaruk <i.charge92@gmail.com>
2023-01-11 15:47:43 +0200

    Friends are ready

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,14 +1,27 @@
+import PropTypes from 'prop-types';
 import css from "./FriendsList.module.css";
 
-export const FriendsList = ({friends}) => {
-  return <ul className={css.friend_list}>
+export const FriendsList = ({ friends }) => {
+  return <section className={css.friends}><ul className={css.friend_list}>
     {friends.map(({ avatar, name, isOnline, id }) => (
       <li className={css.item}
         key={id}>
-        <span className={`${css.status} ${css[isOnline]}`}></span>
-  <img className={css.avatar} src={avatar} alt={`${name} avatar`} width="48" />
+        <span className={isOnline ? css.online : css.offline}></span>
+        <img className={css.avatar} src={avatar} alt={`${name} avatar`} width="48" />
         <p className={css.name}>{name}</p>
-</li>
+      </li>
     ))}
-</ul>
-}
+  </ul>
+</section>
+};
+
+
+
+FriendsList.propTypes = {
+  friends: PropTypes.arrayOf(PropTypes.exact({
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+    id: PropTypes.string.isRequired,
+  }))
+};
