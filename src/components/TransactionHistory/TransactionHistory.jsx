421b94d2dbb5d4938d21453d10180cc19ad74d38
AlinaShklyaruk <i.charge92@gmail.com>
2023-01-11 18:11:06 +0200

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -23,3 +23,12 @@ export const TransactionHistory = ({ items }) => {
     </table>
     </section>
 };
+
+TransactionHistory.propTypes = {
+    items: PropTypes.arrayOf(PropTypes.exact({
+        id: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        amount: PropTypes.string.isRequired,
+        currency: PropTypes.string.isRequired,
+    }))
+};
